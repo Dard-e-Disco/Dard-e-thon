aa456933cbff83b0ddd7fe79688d7b0b6fbe9211
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2023-01-21 17:00:55 +0530

    .

--- ktjfe/src/App.js
+++ ktjfe/src/App.js
@@ -1,36 +1,23 @@
-import React, { useState } from 'react';
-import Event from"./Components/Events/Event"
-
+import React, { useState } from "react";
+import Event from "./Components/Events/Event";
 import "./App.css";
-import UserContext from "./Context/UserContext";
+
 import Modal2 from "./Components/Login copy/components/Modal.js";
 
 function App() {
-  // const navigate = useNavigate();
-
   //login-signup
   const [loginSignup, setLoginSignup] = useState(false);
-
   const toggleLoginModal = () => {
     setLoginSignup(!loginSignup);
   };
 
-
-
-
   return (
     <div className="App">
-      {/* <Routes>
-
-      </Routes> */}
-      {loginSignup && (
-        <Modal2 setOpenModal={toggleLoginModal} />
-      )}
-
+      {loginSignup && <Modal2 setOpenModal={toggleLoginModal} />}
       <button className="button-landingpage" onClick={toggleLoginModal}>
         Login/Register
       </button>
-      <Event/>
+      <Event />
     </div>
   );
 }
