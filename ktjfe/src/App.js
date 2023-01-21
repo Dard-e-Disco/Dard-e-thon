216126ee96eef687eef1c694a5b3c1a529f3a140
TejalBalyan <108823959+TejalBalyan@users.noreply.github.com>
2023-01-21 16:56:52 +0530

    .

--- ktjfe/src/App.js
+++ ktjfe/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Modal2 from '../src/Components/Login copy/components/Modal';
+// import Modal2 from '../src/Components/Login copy/components/Modal';
+import Events from "./Components/Events/Event"
 
 function App() {
   return (
     <div className="App">
-      {true && (
+      {/* {true && (
         <Modal2 showToast={showToast} setOpenModal={toggleLoginModal} />
-      )}
+      )} */}
+      {/* Hello */}
+      <Events/>
     </div>
   );
 }
