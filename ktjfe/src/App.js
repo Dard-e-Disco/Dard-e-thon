fd49316d559b8282fd7a471addc0daf45706e334
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2023-01-21 17:00:09 +0530

    .

--- ktjfe/src/App.js
+++ ktjfe/src/App.js
@@ -4,41 +4,10 @@ import React, { useState } from 'react';
 import "./App.css";
 import UserContext from "./Context/UserContext";
 import Modal2 from "./Components/Login copy/components/Modal.js";
-import Toast from "./Components/Toast/Toast";
 
 function App() {
   // const navigate = useNavigate();
-  const [eveToast, setEveToast] = useState(false);
-  const [toast, setToast] = useState({
-    istrue: false,
-    message: "",
-    type: "",
-  });
 
-  function showToast(ist, msg, type) {
-    setToast({
-      istrue: false,
-      message: "",
-      type: "",
-    });
-    setTimeout(() => {
-      setToast({
-        istrue: ist,
-        message: msg,
-        type: type,
-      });
-    }, 10);
-    const mytimeout = setTimeout(() => {
-      setToast({
-        istrue: false,
-        message: "",
-        type: "",
-      });
-    }, 5000);
-    return () => {
-      clearTimeout(mytimeout);
-    };
-  }
   //login-signup
   const [loginSignup, setLoginSignup] = useState(false);
 
@@ -55,7 +24,7 @@ function App() {
 
       </Routes> */}
       {loginSignup && (
-        <Modal2 showToast={showToast} setOpenModal={toggleLoginModal} />
+        <Modal2 setOpenModal={toggleLoginModal} />
       )}
 
       <button className="button-landingpage" onClick={toggleLoginModal}>
