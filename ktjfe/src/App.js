15b99e7a5a74dbbae58a92cc32e04449716fcd98
dev-064 <divyanshujha112@gmail.com>
2023-01-22 13:03:41 +0530

    .

--- ktjfe/src/App.js
+++ ktjfe/src/App.js
@@ -3,16 +3,18 @@ import Landingpage from './Components/Landingpage/Landingpage'
 import PostedEvents from './Components/PostedEvents/Postedevents'
 import EventRequests from './Components/EventRequests/Eventrequests'
 import AllEvents from './Components/AllEvents/Allevents'
+import Login from './Components/Login_Signin/Login'
 import { Routes,Route } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <Routes>
+      <Login/>
+      {/* <Routes>
         <Route exact path="/" element={<Landingpage/>}/>
         <Route exact path="/postedEvents" element={<PostedEvents/>}/>
         <Route exact path="/requests" element={<EventRequests/>}/>
         <Route exact path="/allevents" element={<AllEvents/>}/>
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
---
+++ ktjfe/src/Components/Login_Signin/Login.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+const Login = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid Email").required("Required"),
+      password: Yup.string()
+        .min(6, "Password length must be atleast than 8 letters")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      axios
+        .post("https://localhost:5000/api/auth/login", {
+          email: formik.values.email,
+          password: formik.values.password,
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error.message);
+        });
+    },
+  });
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="inputfield">
+          <span>{/* <FaIcons.FaUserTie /> */}</span>
+          <label>EMAIL</label>
+          <input
+            name="email"
+            type="email"
+            required
+            autoComplete="off"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+        </div>
+        <div className="inputfield" id="signin-password">
+          <span>{/* <RiIcons.RiLockPasswordFill /> */}</span>
+          <label>PASSWORD</label>
+          <input
+            name="password"
+            type="password"
+            required
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+        </div>
+        <button className="sgnbutton" id="sign-in-button-id" type="submit">
+          Sign In
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
