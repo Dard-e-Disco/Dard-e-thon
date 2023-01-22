55fc7acc6577f011f2f2ba611da8af06fe57576d
dev-064 <divyanshujha112@gmail.com>
2023-01-22 12:42:45 +0530

    .

--- ktjfe/src/App.js
+++ ktjfe/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Landingpage from './Components/Landingpage/Landingpage'
-
+import PostedEvents from './Components/PostedEvents/Postedevents'
+import EventRequests from './Components/EventRequests/Eventrequests'
+import AllEvents from './Components/AllEvents/Allevents'
+import { Routes,Route } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <Landingpage/>
+      <Routes>
+        <Route exact path="/" element={<Landingpage/>}/>
+        <Route exact path="/postedEvents" element={<PostedEvents/>}/>
+        <Route exact path="/requests" element={<EventRequests/>}/>
+        <Route exact path="/allevents" element={<AllEvents/>}/>
+      </Routes>
     </div>
   )
 }
