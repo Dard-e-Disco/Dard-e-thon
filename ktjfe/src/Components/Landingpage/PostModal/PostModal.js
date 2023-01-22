68354fb1cbb4c62e6efefd76d417759ca9a8ef5b
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2023-01-22 15:13:10 +0530

    .

--- ktjfe/src/Components/Landingpage/PostModal/PostModal.js
+++ ktjfe/src/Components/Landingpage/PostModal/PostModal.js
@@ -1,24 +1,55 @@
 import React from "react";
 import "./PostModal.css";
 import { useFormik } from "formik";
+import axios from "axios";
 import * as Yup from "yup";
 function PostModal(props) {
   const formik = useFormik({
     initialValues: {
-      evename: "",
-      evedisc: "",
-      parti: "",
+      EventName: "",
+      desc: "",
+      np: "",
     },
     validationSchema: Yup.object({
-      evename: Yup.string().required("Required"),
-      evedisc: Yup.string().required("Required"),
-      parti: Yup.number()
-        .min(2, "Enter A valid participant number")
-        .typeError("That doesn't look like a participant number")
-        .integer("A participant number can't include a decimal point")
+      EventName: Yup.string().required("Required"),
+      desc: Yup.string().required("Required"),
+      np: Yup.number()
+        .min(2, "Enter A valid npcipant number")
+        .typeError("That doesn't look like a npcipant number")
+        .integer("A npcipant number can't include a decimal point")
         .required("Required"),
     }),
-    onSubmit: (values) => {},
+    onSubmit: (values) => {
+      axios
+        .post(
+          "http://localhost:5000/api/event/PostEvent",
+          {
+            "CreatorID": "63cce87389f1e9f79fc976a5",
+            EventName: formik.values.EventName,
+            desc: formik.values.desc,
+            np: formik.values.np,
+            npremaining: formik.values.np,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token":
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzY2NlODczODlmMWU5Zjc5ZmM5NzZhNSIsImlhdCI6MTY3NDM3Nzc1OH0.RiHQ4XxhaoBPmnOl_RPOEQMQZSOXtItUyIpR4QFGEfc",
+            },
+          }
+        )
+        .then(function (response) {
+          if (response.data.code === 0) {
+            console.log("successful");
+            
+          } else {
+          }
+          return response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
   });
   return (
     props.show && (
@@ -44,69 +75,69 @@ function PostModal(props) {
         >
           <form onSubmit={formik.handleSubmit} className="profile">
             <div className="inputfield">
-              <label for="evename" class="">
+              <label for="EventName" class="">
                 EVENT NAME
               </label>
               <input
                 type="text"
-                id="evename"
+                id="EventName"
                 class=""
                 required
                 autoComplete="off"
-                value={formik.values.evename}
+                value={formik.values.EventName}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
             </div>
-            {formik.touched.evename && formik.errors.evename ? (
+            {formik.touched.EventName && formik.errors.EventName ? (
               <p class="mt-2 text-sm text-right text-red-600 dark:text-red-500">
-                <span class="font-medium">{formik.errors.evename}</span>
+                <span class="font-medium">{formik.errors.EventName}</span>
               </p>
             ) : (
               <br />
             )}
 
             <div className="inputfield">
-              <label for="evedisc" class="">
+              <label for="desc" class="">
                 EVENT DISCRIPTION
               </label>
               <input
                 type="text"
-                id="evedisc"
+                id="desc"
                 class=""
                 required
                 autoComplete="off"
-                value={formik.values.evedisc}
+                value={formik.values.desc}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
             </div>
-            {formik.touched.evedisc && formik.errors.evedisc ? (
+            {formik.touched.desc && formik.errors.desc ? (
               <p class="mt-2 text-sm text-right text-red-600 dark:text-red-500">
-                <span class="font-medium">{formik.errors.evedisc}</span>
+                <span class="font-medium">{formik.errors.desc}</span>
               </p>
             ) : (
               <br />
             )}
 
             <div className="inputfield">
-              <label for="parti" class="">
+              <label for="np" class="">
                 NUMBER OF PARTICIPANT
               </label>
               <input
                 type="text"
-                id="parti"
+                id="np"
                 class=""
                 required
                 autoComplete="off"
-                value={formik.values.parti}
+                value={formik.values.np}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
             </div>
-            {formik.touched.parti && formik.errors.parti ? (
+            {formik.touched.np && formik.errors.np ? (
               <p class="mt-2 text-sm text-right text-red-600 dark:text-red-500">
-                <span class="font-medium">{formik.errors.parti}</span>
+                <span class="font-medium">{formik.errors.np}</span>
               </p>
             ) : (
               <br />
