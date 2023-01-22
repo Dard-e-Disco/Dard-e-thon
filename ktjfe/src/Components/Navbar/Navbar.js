2c700264f872ba6b30eeee45379eef39ba74f776
rraj-03 <ritikraj1369@gmail.com>
2023-01-22 17:22:13 +0530

    .

--- ktjfe/src/Components/Navbar/Navbar.js
+++ ktjfe/src/Components/Navbar/Navbar.js
@@ -5,14 +5,13 @@ const Navbar = (props) => {
     <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <img src="https://m.springfest.in/static/media/SF%20logo.3df9998d1cc066ea9bd5.png" className='w-16 h-16 text-white p-2 rounded-full' alt="" />
-        <span class="ml-3 text-xl">SPRINGFEST</span>
+        <img src="https://i.pinimg.com/originals/0a/1f/82/0a1f820e29719c7b67e9d5aa44241155.png" className='w-16 h-16 text-white p-2 rounded-full' alt="" />
+        <span class="ml-3 text-xl">DARD</span>
       </a>
       <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
         {props.isloggedin&&<Link to={"/requests"} class="mr-5 hover:text-gray-900">Dashboasrd</Link>}
-        <Link  class="mr-5 hover:text-gray-900">PostedEvents</Link>
-        <a class="mr-5 hover:text-gray-900">Third Link</a>
-        <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+        <Link to={"/postedEvents"}  class="mr-5 hover:text-gray-900">PostedEvents</Link>
+        
       </nav>
       <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" onClick={() => {
               props.ShowLogin_Modal();
