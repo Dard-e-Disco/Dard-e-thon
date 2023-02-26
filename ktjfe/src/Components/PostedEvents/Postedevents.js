8e2ec68f3fb559ef90a41b20dc264d6e7c81214e
dev-064 <divyanshujha112@gmail.com>
2023-02-26 14:47:57 +0530

    .

--- ktjfe/src/Components/PostedEvents/Postedevents.js
+++ ktjfe/src/Components/PostedEvents/Postedevents.js
@@ -1,56 +1,52 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import "./PostedEvent.css"
+import "./PostedEvent.css";
 const Postedevents = () => {
   const [events, setEvents] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/event/getAllEvents")
       .then((response) => {
-        setEvents(response.data);
+        setEvents(response.data.filter(function(element){
+          
+        }));
       });
   }, []);
-  const JoinRequest=(eventID)=>{
-    axios.post("http://localhost:5000/api/event/EventJoinRequest",{
-      EventID:eventID,
-      UserID:JSON.parse(localStorage.getItem("user"))._id
-  },{
-        headers:{
-            'auth-token':localStorage.getItem("token")
+  const JoinRequest = (eventID) => {
+    axios
+      .post(
+        "http://localhost:5000/api/event/EventJoinRequest",
+        {
+          EventID: eventID,
+          UserID: JSON.parse(localStorage.getItem("user"))._id,
+        },
+        {
+          headers: {
+            "auth-token": localStorage.getItem("token"),
+          },
         }
-    },).then(response=>{
-      console.log('response', response)
-      if(response.data.code===0){
-        alert("Your request is sent successfully")
-      }
-    })
-  }
+      )
+      .then((response) => {
+        console.log("response", response);
+        if (response.data.code === 0) {
+          alert("Your request is sent successfully");
+        }
+      });
+  };
 
   return (
     <>
-      
-          <section class="text-gray-600 body-font overflow-hidden">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="-my-8 divide-y-2 divide-gray-100">
-    {events.map((element,index) => {
-        return (<div class="py-8 flex flex-wrap md:flex-nowrap">
-        
-        <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{element.EventName}</h2>
-          <p class="leading-relaxed">{element.desc}</p>
-          <button class="text-indigo-500 inline-flex items-center mt-4" onClick={()=>{
-            JoinRequest(element._id)
-          }}>Request To Join
-          </button>
+      <section className="Posted-Events-Parent">
+        <h1>Events</h1>
+      {events.map((element)=>{
+        return <div className="event">
+          <h2>Lorem, ipsum dolor.</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae laudantium adipisci sed placeat perspiciatis fugit iure atque dolore ratione similique.</p>
+          <button className="primary-btn primary-btn_ripple">Request</button>
         </div>
-      </div> );
       })}
-   
-    </div>
-  </div>
-</section>
-       
+      </section>
     </>
   );
 };
